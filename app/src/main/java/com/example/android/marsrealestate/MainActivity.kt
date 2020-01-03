2b32355f23a650ff1f297299c53676d41a01923e
taruc <limew-pm17@student.tarc.edu.my>
2020-01-03 13:49:01 +0800

    init

--- app/src/main/java/com/example/android/marsrealestate/MainActivity.kt
+++ app/src/main/java/com/example/android/marsrealestate/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
     /**
      * Our MainActivity is only responsible for setting the content view that contains the
      * Navigation Host.
-     *///
+     *//
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
